9d353a358859ab3cc16d3771906bb255c62dd545
Javier Farre <jfarre90@gmail.com>
2019-02-21 12:33:42 +0000

    added API request and outputting what we get in the console.log

---
+++ javascript.js
@@ -0,0 +1,14 @@
+var btn = document.querySelector("#btn");
+    // listen for clicks
+    btn.addEventListener("click", function(){
+        // make changes
+        var XHR = new XMLHttpRequest();
+        
+        XHR.onreadystatechange = function (){
+            if(XHR.readyState ==4 && XHR.status ==200) {
+                console.log(XHR.responseText);
+            }
+        };
+        XHR.open("GET", "https://dog.ceo/api/breeds/image/random");
+        XHR.send;
+    });
